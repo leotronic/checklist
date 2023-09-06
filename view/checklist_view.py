865cced8352a7b40c2659e9813747e4c0b198eb3
Leonard Schiel <leonard.schiel@rwth-aachen.de>
2023-09-06 13:45:23 +0300

    new Item button.

--- view/checklist_view.py
+++ view/checklist_view.py
@@ -1,5 +1,11 @@
 from tkinter import Tk, Frame, Checkbutton, TOP, Button, LEFT
 
+FULL_WIDTH = "x"
+
+STICK_LEFT = "W"
+
+STICK_RIGHT = "E"
+
 
 class ChecklistView:
     def __init__(self, controller, checklist):
@@ -11,8 +17,13 @@ class ChecklistView:
         self.checklist = checklist
 
         self.frame = Frame(self.window)
-        self.frame.pack(fill="both", expand=True)
+        self.frame.pack(fill="both", expand=True, side='top')
         self.frame.grid_columnconfigure(0, weight=1)
+
+        self.new_item_button = Button(self.window, text="New Item",
+                                      command=self.add_new_item)
+        self.new_item_button.pack(side='bottom',
+                                  fill=FULL_WIDTH)
 
     def update_view(self):
         for widget in self.frame.winfo_children():
@@ -21,10 +32,14 @@ class ChecklistView:
         for index, item in enumerate(self.checklist.items):
             checkbutton = Checkbutton(self.frame, text=item.description,
                                       command=item.toggle)
-            checkbutton.grid(row=index, column=0, sticky="W")
+            checkbutton.grid(row=index, column=0, sticky=STICK_LEFT)
             delete_button = Button(self.frame, text="🗑", command=lambda
-                item_to_remove=item: self.controller.remove_item(item_to_remove))
-            delete_button.grid(row=index, column=1, sticky="E")
+                item_to_remove=item: self.controller.remove_item(
+                item_to_remove))
+            delete_button.grid(row=index, column=1, sticky=STICK_RIGHT)
+
+    def add_new_item(self):
+        print("Add new item!")
 
     def mainloop(self):
         self.window.mainloop()
